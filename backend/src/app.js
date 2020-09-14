6bceafd4cae29a46ab1790e55bda37199afab75a
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-14 20:08:38 -0300

    conexão com banco de dados mongo

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 
 //import modulos
 const express = require('express');
+const mongoose = require('mongoose');
 
 class App {
 
@@ -8,6 +9,17 @@ class App {
     {
         //instanciar express
         this.server = express();
+
+        //connect banco de daodos
+        this.dataBase();
+    }
+
+    //construtor para conectar ao banco de dados
+    dataBase()
+    {
+        mongoose.connect("mongodb+srv://pitolo:HqxldE4C6d1dbtb6@cluster0.aiiwo.mongodb.net/piloto?retryWrites=true&w=majority",
+        { useNewUrlParser: true, useUnifiedTopology: true}
+        );
     }
 }
 
