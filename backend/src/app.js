a16a46639b156118a6e8b0a4a13667aa2a15ca6b
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-09-14 20:38:27 -0300

    add construtores

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
 
 //import modulos
-const express = require('express');
-const mongoose = require('mongoose');
+const express   = require('express');
+const mongoose  = require('mongoose');
+const routes    = require('./routes');
 
 class App {
 
@@ -12,6 +13,9 @@ class App {
 
         //connect banco de daodos
         this.dataBase();
+
+        //rotas 
+        this.routes();
     }
 
     //construtor para conectar ao banco de dados
@@ -20,6 +24,12 @@ class App {
         mongoose.connect("mongodb+srv://pitolo:HqxldE4C6d1dbtb6@cluster0.aiiwo.mongodb.net/piloto?retryWrites=true&w=majority",
         { useNewUrlParser: true, useUnifiedTopology: true}
         );
+    }
+
+    //construtor para rotas
+    routes()
+    {
+        this.server.use(routes);
     }
 }
 
